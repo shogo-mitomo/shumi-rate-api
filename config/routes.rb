3e58952972b7c635278ce49a7b3a81e22f16d223
shogo-mitomo <shogo.mitomo@monstar-lab.com>
2016-11-27 01:08:05 +0900

    rake db:migrate && erd

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
+# == Route Map
+#
+#            Prefix Verb URI Pattern  Controller#Action
+# komachi_heartbeat      /healthcheck KomachiHeartbeat::Engine
+#
+# Routes for KomachiHeartbeat::Engine:
+#    heartbeat GET  /heartbeat(.:format)    komachi_heartbeat/heartbeat#index {:format=>"txt"}
+#      version GET  /version(.:format)      komachi_heartbeat/heartbeat#version
+# stats_worker GET  /stats/worker(.:format) komachi_heartbeat/stats#worker
+#
+
 Rails.application.routes.draw do
   mount KomachiHeartbeat::Engine => '/healthcheck'
 end

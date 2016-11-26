52a63c5d19f3107015b902f5bf89c1d63ac8e803
shogo-mitomo <shogo.mitomo@monstar-lab.com>
2016-11-27 00:12:21 +0900

    Fix unicorn settings for Heroku

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -19,40 +19,40 @@ worker_processes 4
 # user, do this to switch euid/egid in the workers (also chowns logs):
 # user "unprivileged_user", "unprivileged_group"
 
-# Help ensure your application will always spawn in the symlinked
-# "current" directory that Capistrano sets up.
-working_directory '/path/to/app/current' # available in 0.94.0+
+# # Help ensure your application will always spawn in the symlinked
+# # "current" directory that Capistrano sets up.
+# working_directory '/path/to/app/current' # available in 0.94.0+
 
-# listen on both a Unix domain socket and a TCP port,
-# we use a shorter backlog for quicker failover when busy
-listen '/path/to/.unicorn.sock', backlog: 64
-listen 8080, tcp_nopush: true
+# # listen on both a Unix domain socket and a TCP port,
+# # we use a shorter backlog for quicker failover when busy
+# listen '/path/to/.unicorn.sock', backlog: 64
+# listen 8080, tcp_nopush: true
 
 # nuke workers after 30 seconds instead of 60 seconds (the default)
 timeout 30
 
-# feel free to point this anywhere accessible on the filesystem
-pid '/path/to/app/shared/pids/unicorn.pid'
+# # feel free to point this anywhere accessible on the filesystem
+# pid '/path/to/app/shared/pids/unicorn.pid'
 
-# By default, the Unicorn logger will write to stderr.
-# Additionally, ome applications/frameworks log to stderr or stdout,
-# so prevent them from going to /dev/null when daemonized here:
-stderr_path '/path/to/app/shared/log/unicorn.stderr.log'
-stdout_path '/path/to/app/shared/log/unicorn.stdout.log'
+# # By default, the Unicorn logger will write to stderr.
+# # Additionally, ome applications/frameworks log to stderr or stdout,
+# # so prevent them from going to /dev/null when daemonized here:
+# stderr_path '/path/to/app/shared/log/unicorn.stderr.log'
+# stdout_path '/path/to/app/shared/log/unicorn.stdout.log'
 
 # combine Ruby 2.0.0+ with "preload_app true" for memory savings
 preload_app true
 
-# Enable this flag to have unicorn test client connections by writing the
-# beginning of the HTTP headers before calling the application.  This
-# prevents calling the application for connections that have disconnected
-# while queued.  This is only guaranteed to detect clients on the same
-# host unicorn runs on, and unlikely to detect disconnects even on a
-# fast LAN.
-check_client_connection false
+# # Enable this flag to have unicorn test client connections by writing the
+# # beginning of the HTTP headers before calling the application.  This
+# # prevents calling the application for connections that have disconnected
+# # while queued.  This is only guaranteed to detect clients on the same
+# # host unicorn runs on, and unlikely to detect disconnects even on a
+# # fast LAN.
+# check_client_connection false
 
-# local variable to guard against running a hook multiple times
-run_once = true
+# # local variable to guard against running a hook multiple times
+# run_once = true
 
 before_fork do |_server, _worker|
   # the following is highly recomended for Rails + "preload_app true"

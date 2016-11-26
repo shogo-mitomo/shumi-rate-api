0c9046f663fa0985f9aa353a43c5261a1ad1e0d2
shogo-mitomo <shogo.mitomo@monstar-lab.com>
2016-11-27 02:36:26 +0900

    rails g controller prefectures && rails g serializer prefectures

---
+++ app/serializers/prefectures_serializer.rb
@@ -0,0 +1,3 @@
+class PrefecturesSerializer < ActiveModel::Serializer
+  attributes :id
+end
---
+++ spec/controllers/prefectures_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe PrefecturesController, type: :controller do
+
+end

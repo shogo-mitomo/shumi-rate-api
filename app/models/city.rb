5c532827f519ad3060c23c674e1538d88bc1e43f
shogo-mitomo <shogo.mitomo@monstar-lab.com>
2016-11-27 01:47:59 +0900

    rake db:migrate && erd

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,3 +1,25 @@
+# == Schema Information
+#
+# Table name: cities
+#
+#  id             :integer          not null, primary key
+#  code           :string           not null
+#  name           :string           not null
+#  big_city_flag  :integer          not null
+#  prefectures_id :integer          not null
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
+# Indexes
+#
+#  index_cities_on_code            (code) UNIQUE
+#  index_cities_on_prefectures_id  (prefectures_id)
+#
+# Foreign Keys
+#
+#  fk_rails_584d859bfb  (prefectures_id => prefectures.id)
+#
+
 class City < ApplicationRecord
   belongs_to :prefectures
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161126160238) do
+ActiveRecord::Schema.define(version: 20161126161059) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cities", force: :cascade do |t|
+    t.string   "code",           null: false
+    t.string   "name",           null: false
+    t.integer  "big_city_flag",  null: false
+    t.integer  "prefectures_id", null: false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.index ["code"], name: "index_cities_on_code", unique: true, using: :btree
+    t.index ["prefectures_id"], name: "index_cities_on_prefectures_id", using: :btree
+  end
 
   create_table "prefectures", force: :cascade do |t|
     t.integer  "code",       null: false
@@ -23,4 +34,5 @@ ActiveRecord::Schema.define(version: 20161126160238) do
     t.index ["code"], name: "index_prefectures_on_code", unique: true, using: :btree
   end
 
+  add_foreign_key "cities", "prefectures", column: "prefectures_id"
 end

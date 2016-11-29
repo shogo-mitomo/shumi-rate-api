42a197aeacdd1ac4419c66718372fca0d335aaf0
shogo-mitomo <shogo.mitomo@monstar-lab.com>
2016-11-30 04:35:54 +0900

    Update cors.rb to use Settings

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
-
-    resource '*',
-      headers: :any,
-      methods: %i(get post put patch delete options head)
+    origins Settings.domain
+    resource '*', headers: :any,
+                  methods: %i(get post put patch delete options head)
   end
 end

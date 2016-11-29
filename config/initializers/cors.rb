ce463368f7b2d7ffa1c7e71f8c2bba831ab38125
shogo-mitomo <shogo.mitomo@monstar-lab.com>
2016-11-30 04:46:43 +0900

    Revert "Update cors.rb to use Settings"
    This reverts commit 42a197aeacdd1ac4419c66718372fca0d335aaf0.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins Settings.domain
-    resource '*', headers: :any,
-                  methods: %i(get post put patch delete options head)
+    origins '*'
+
+    resource '*',
+      headers: :any,
+      methods: %i(get post put patch delete options head)
   end
 end
